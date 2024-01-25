a4bfeaa5cf394582a2a3357199810fddadda215e
Samuele Calamita <samuele.calamita@diva-e.com>
2024-01-25 22:48:12 +0100

    improve accordion

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,11 +1,84 @@
-/*
- * Accordion Block
- * Recreate an accordion
- * https://www.hlx.live/developer/block-collection/accordion
+/**
+ * TODO: 
+ * - add svg icon to summary with js (so it can be styled with css) 
  */
 
-function hasWrapper(el) {
-  return !!el.firstElementChild && window.getComputedStyle(el.firstElementChild).display === 'block';
+class Accordion {
+  constructor(el) {
+    this.details = el;
+    this.summary = el.querySelector('summary');
+    this.content = el.querySelector('.accordion-item-body');
+    this.animation = null;
+    this.isClosing = false;
+    this.isExpanding = false;
+    this.summary.addEventListener('click', (e) => this.onClick(e));
+  }
+
+  onClick(e) {
+    e.preventDefault();
+    this.details.style.overflow = 'hidden';
+
+    if (this.isClosing || !this.details.open) {
+      this.open();
+    } else if (this.isExpanding || this.details.open) {
+      this.shrink();
+    }
+  }
+
+  shrink() {
+    this.isClosing = true;
+    
+    const startHeight = `${this.details.offsetHeight}px`;
+    const endHeight = `${this.summary.offsetHeight}px`;
+    
+    if (this.animation) {
+      this.animation.cancel();
+    }
+    
+    this.animation = this.details.animate({
+      height: [startHeight, endHeight]
+    }, {
+      duration: 400,
+      easing: 'ease-in-out'
+    });
+    
+    this.animation.onfinish = () => this.onAnimationFinish(false);
+    this.animation.oncancel = () => this.isClosing = false;
+  }
+
+  open() {
+    this.details.style.height = `${this.details.offsetHeight}px`;
+    this.details.open = true;
+    window.requestAnimationFrame(() => this.expand());
+  }
+
+  expand() {
+    this.isExpanding = true;
+    const startHeight = `${this.details.offsetHeight}px`;
+    const endHeight = `${this.summary.offsetHeight + this.content.offsetHeight}px`;
+    
+    if (this.animation) {
+      this.animation.cancel();
+    }
+    
+    this.animation = this.details.animate({
+      height: [startHeight, endHeight]
+    }, {
+      duration: 400,
+      easing: 'ease-out'
+    });
+
+    this.animation.onfinish = () => this.onAnimationFinish(true);
+    this.animation.oncancel = () => this.isExpanding = false;
+  }
+
+  onAnimationFinish(open) {
+    this.details.open = open;
+    this.animation = null;
+    this.isClosing = false;
+    this.isExpanding = false;
+    this.details.style.height = this.details.style.overflow = '';
+  }
 }
 
 export default function decorate(block) {
@@ -15,19 +88,16 @@ export default function decorate(block) {
     const summary = document.createElement('summary');
     summary.className = 'accordion-item-label';
     summary.append(...label.childNodes);
-    if (!hasWrapper(summary)) {
-      summary.innerHTML = `<p>${summary.innerHTML}</p>`;
-    }
+
     // decorate accordion item body
     const body = row.children[1];
     body.className = 'accordion-item-body';
-    if (!hasWrapper(body)) {
-      body.innerHTML = `<p>${body.innerHTML}</p>`;
-    }
     // decorate accordion item
     const details = document.createElement('details');
     details.className = 'accordion-item';
     details.append(summary, body);
     row.replaceWith(details);
+
+    new Accordion(details);
   });
 }
